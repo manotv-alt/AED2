cfb56404ea18f959d05847f869f044d146dd55ee
manotv-alt <manotvloco0@gmail.com>
2024-10-27 00:50:09 -0300

    Temporary update

---
+++ Verificador de Código C/exemplo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    if (a > 0) {
+        printf("Positivo\n");
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
